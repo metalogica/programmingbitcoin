9f4da99f12046f8ea4f064f2eec757b4ce2637d8
richard jarram <rjarram@me.com>
2021-07-04 19:50:23 -0700

    completed ch1

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -39,7 +39,8 @@ class FieldElement:
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
@@ -47,7 +48,8 @@ class FieldElement:
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     # tag::source3[]
     def __pow__(self, exponent):
@@ -64,7 +66,9 @@ class FieldElement:
         # this means:
         # 1/n == pow(n, p-2, p)
         # We return an element of the same class
-        raise NotImplementedError
+        a = self.num
+        b = pow(other.num, self.prime-2, self.prime)
+        return self.__class__(a * b % self.prime, self.prime)
 
 
 class FieldElementTest(TestCase):
